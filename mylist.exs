8fb3171f3ccb176e34cb87448cb4b59aecd4f26b
Yukio Ueda <triangle.pillow@gmail.com>
2020-02-14 23:46:15 +0900

    created myList

---
+++ mylist.exs
@@ -0,0 +1,31 @@
+defmodule MyList do
+  def len([]), do: 0
+  def len([_head | tail]), do: 1 + len(tail)
+  def square([]), do: []
+  def square([head | tail]), do: [head * head | square(tail)]
+  def add_1([]), do: []
+  def add_1([head | tail]), do: [head + 1 | add_1(tail)]
+  def map([], _func), do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
+  # def sum([], total), do: total
+  # def sum([head | tail], total), do: sum(tail, head + total)
+  # def sum(list), do: _sum(list, 0)
+  # defp _sum([], total), do: total
+  # defp _sum([head | total], total), do: _sum(total, head + total)
+  def sum([head | []]), do: head
+  def sum([head | [head1 | tail]]), do: sum([head + head1 | tail])
+  def sum([head | tail]), do: head + tail
+  def reduce([], value, _), do: value
+  def reduce([head | tail], value, func), do: reduce(tail, func.(head, value), func)
+  def mapsum(list, func), do: _mapsum(list, func, 0)
+  defp _mapsum([], _func, total), do: total
+  defp _mapsum([head | tail], func, total), do: _mapsum(tail, func, total + func.(head))
+  def max([head | []]), do: head
+  def max([head | [head1 | tail]]) when head < head1, do: max([head1 | tail])
+  def max([head | [head1 | tail]]) when head > head1, do: max([head | tail])
+  def max([head | tail]) when head > tail, do: max([head | []])
+  def max([head | tail]) when head < tail, do: max([tail | []])
+  def caesar([], _num), do: []
+  def caesar([head | tail], num) when head + num > 122, do: [head + num - 26 | caesar(tail, num)]
+  def caesar([head | tail], num) when head + num < 122, do: [head + num | caesar(tail, num)]
+end
