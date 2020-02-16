6ca4c821bf271d29b97900601ad84e2f371a9a45
Yukio Ueda <triangle.pillow@gmail.com>
2020-02-17 01:57:56 +0900

    solved center

--- string_binary.exs
+++ string_binary.exs
@@ -16,4 +16,26 @@ defmodule StringAndBinaries do
       false
     end
   end
+
+  def center(list) do
+    max = _calc_max_length(list, 0)
+
+    Enum.each(list, fn word -> IO.puts(_padding(word, max)) end)
+  end
+
+  defp _calc_max_length([], max_length), do: max_length
+
+  defp _calc_max_length([head | tail], max_length) do
+    if max_length < String.length(head) do
+      _calc_max_length(tail, String.length(head))
+    else
+      _calc_max_length(tail, max_length)
+    end
+  end
+
+  defp _padding(string, max) do
+    string
+    |> String.pad_leading(div(max - String.length(string), 2) + String.length(string))
+    |> String.pad_trailing(max)
+  end
 end
