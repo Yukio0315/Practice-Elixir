98897443d3ba2b3729d2074b2212f3a0872af986
Yukio Ueda <triangle.pillow@gmail.com>
2020-02-18 21:59:53 +0900

    added exdoc

--- issues/mix.exs
+++ issues/mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/Yukio0315/Practice-Elixir",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -22,9 +24,10 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.6"},
-      {:poison, "~> 3.1"}
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      httpoison: "~> 1.6",
+      poison: "~> 3.1",
+      ex_doc: "~> 0.21",
+      earmark: "~> 1.4.3"
     ]
   end
 
