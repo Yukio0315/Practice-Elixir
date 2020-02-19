e3004826e03cae86bbcf4322aceabe31a3c64c04
Yukio Ueda <triangle.pillow@gmail.com>
2020-02-19 18:49:11 +0900

    changed database setting

--- programming_ecto/code/config/config.exs
+++ programming_ecto/code/config/config.exs
@@ -1,11 +1,11 @@
-#---
+# ---
 # Excerpted from "Programming Ecto",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/wmecto for more book information.
-#---
+# ---
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
@@ -31,14 +31,9 @@ use Mix.Config
 
 config :music_db, :ecto_repos, [MusicDB.Repo]
 
-config :music_db, MusicDB.Repo,
-  # username: your_username,
-  # password: your_password,
-  database: "music_db",
-  hostname: "localhost",
-  migration_lock: nil # this is not normally needed - we put it here to support an example of
-                      # creating an index with the `concurrently` option set to true
+# creating an index with the `concurrently` option set to true
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
+import_config "dev.secret.exs"
